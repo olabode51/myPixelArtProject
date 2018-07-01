d89e2c87223fd1f63e737a9ddd600b4b604e9095
olabode51 <olabode7179@gmail.com>
2018-07-01 15:49:32 +0100

    updatedArtMaker

--- designs.js
+++ designs.js
@@ -1,17 +1,22 @@
 // Select color input
 // Select size input
 
-
+let width = $('#inputWidth');
+let height =$('#inputHeight');
+let color =$('#colorPicker');
 // When size is submitted by the user, call makeGrid()
+
+
+
 function makeGrid () {
     $("table tr").remove();
 
      let height = $('#inputHeight').val();    //making the height to receive value input
      let width = $('#inputWidth').val();      // making the height to receive value input
     
-    for(let x = 1; x <= height; x++) {
+    for(let N = 1; N <= height; N++) {
         $('table').append('<tr></tr>');
-        for(let y = 1; y <= width; y++) {
+        for(let M = 1; M <= width; M++) {
             $("tr:last").append('<td></td>');
             $("td").attr("class","cell")
         };
@@ -20,17 +25,21 @@ function makeGrid () {
 
 }
 
+$('#clearGrid').click(function() {
+    $('#pixelCanvas').remove();
+})
+
 $('#sizePicker').submit(function (event) {
     event.preventDefault();
     makeGrid();
 });
+
+
+
    
 $('#pixelCanvas').on('click','.cell',function () {
     let color = $('#colorPicker').val();
     $(this).css('background-color',color);
-
-    
 });
-
 
 
